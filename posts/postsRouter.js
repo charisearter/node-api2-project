0e5fcf375019eccdaa72426217967ccb68defcf3
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-07 18:40:30 -0500

    setting up basic syntax for each http request

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -2,7 +2,42 @@ const express = require('express');
 
 const router = express.Router();
 
-//DO http requests here
+//POST	/api/posts
+
+router.post('/', (req, res) => {
+
+});
+
+//POST	/api/posts/:id/comments
+
+router.post('/', (req, res) => {
+  
+});
+
+//GET	/api/posts
+router.get('/', (req, res) => {
+  
+});
+
+//GET	/api/posts/:id
+router.get('/', (req, res) => {
+  
+});
+
+//GET	/api/posts/:id/comments
+router.get('/', (req, res) => {
+  
+});
+
+//DELETE	/api/posts/:id
+router.delete('/', (req, res) => {
+  
+});
+
+//PUT	/api/posts/:id
+router.put('/', (req, res) => {
+  
+});
 
 
 

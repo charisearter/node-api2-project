ae3794411681cde006af5580d1ad28f58cbe364e
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-07 18:29:34 -0500

    fixed express error

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-  res.status(200).json(posts);
-});
+//DO http requests here
+
+
 
 //export default router
 module.exports = router;

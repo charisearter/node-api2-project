f2522da22303d3dcc6329220cfc9d2babc28cb84
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-07 22:33:33 -0500

    most work 2 sorta work and 1 does not will work on it in the morning

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -4,12 +4,11 @@ const db = require('../data/db');
 const router = express.Router();
 
 //POST	/api/posts  Sort of WOrks, gives me an error saying it is not there or there was an error adding it but if I do the get request for the original list, what I added does show up
-
 router.post('/', (req, res) => {
   //const newPost = req.body;
   db.insert(req.body)
   .then( post => {
-    if(!title || !contents){
+    if(!post.title || !post.contents){
       res.status(400).json({ errorMessage: "Please provide title and contents for the post."  });
     } else {
       res.status(201).json(post)
@@ -21,11 +20,23 @@ router.post('/', (req, res) => {
   });
 });
 
-//POST	/api/posts/:id/comments
-
+//POST	/api/posts/:id/comments NOPE
 router.post('/:id/comments', (req, res) => {
-  const { id } = req.params
-  
+  const newComment = req.body;
+  db.insertComment(newComment)
+      .then(comment => {
+        if(!id){
+          res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }else if(!text){
+          res.status(400).json({ errorMessage: "Please provide text for the comment." })
+        }else {
+             res.status(201).json({ data: newComment });
+        }   
+      })
+      .catch(error => {
+          console.log(error);
+          res.status(500).json({ error: "There was an error while saving the comment to the database" });
+      });
 });
 
 //GET	/api/posts WORKS
@@ -91,8 +102,25 @@ router.delete('/:id', (req, res) => {
   })
 });
 
-//PUT	/api/posts/:id
+//PUT	/api/posts/:id  SORTA WORKS Gives error but when do get request does show update
 router.put('/:id', (req, res) => {
+  const changes = req.body;
+  db.update(req.params.id, changes)
+      .then(post => {
+          if (!post) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+              
+          } else if (!post.title || !post.contents) {
+            res.status(400).json({errorMessage: "Please provide title and contents for the post." });
+          }else {
+            res.status(200).json(post);
+          }
+      })
+      .catch(error => {
+          // log error to database
+          console.log(error);
+          res.status(500).json({ error: "The post information could not be modified." });
+      });
   
 });
 
